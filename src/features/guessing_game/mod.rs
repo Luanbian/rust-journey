a66a29a1ce0e3c7c98203ebebf8b699914e49536
Luan Almeida <luan.almeida@jbtec.com.br>
2024-12-27 15:31:03 -0300

    feat: generate secret number

--- src/features/guessing_game/mod.rs
+++ src/features/guessing_game/mod.rs
@@ -1,10 +1,13 @@
 use std::io::{self};
+use rand::Rng;
+
 
 pub fn main() {
+    generate_secret_number();
     get_user_guess();
 }
 
-fn get_user_guess() {
+fn get_user_guess() -> String {
     println!("Guess the number!");
     println!("Please, input your guess");
 
@@ -15,4 +18,11 @@ fn get_user_guess() {
         .expect("Failed to read line");
 
     println!("You guessed: {}", guess);
+    return guess;
+}
+
+fn generate_secret_number() -> u32 {
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {}", secret_number);
+    return secret_number;
 }
